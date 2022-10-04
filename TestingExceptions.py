34154a45213272964b2bd101fb353290df964f07
Marcinosky <102297331+Marcinosky@users.noreply.github.com>
2022-10-04 10:41:09 +0200

    Delete TestingExceptions.py

--- TestingExceptions.py
+++
@@ -1,8 +0,0 @@
-from CollisUL.CollisULCardServer import collisULCardServer
-from LatteLiteEngine_BaseClass import RemoveChip, atClient_init
-
-class ExceptionForced(Exception):
-    Collis = collisULCardServer
-    #RemoveChip(atClient)
-    Collis.handleCollisStopProbe()
-    pass
